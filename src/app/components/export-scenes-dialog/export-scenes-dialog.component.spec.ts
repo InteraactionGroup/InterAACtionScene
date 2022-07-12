c0f83b135fe1f79680da2dabf691ce8b3447d203
Noars <55838787+Noars@users.noreply.github.com>
2022-07-12 17:09:35 +0200

    Fix tests karma

--- src/app/components/export-scenes-dialog/export-scenes-dialog.component.spec.ts
+++ src/app/components/export-scenes-dialog/export-scenes-dialog.component.spec.ts
@@ -13,11 +13,12 @@ describe('ExportScenesDialogComponent', () => {
   let fixture: ComponentFixture<ExportScenesDialogComponent>;
   let dialogRef: jasmine.SpyObj<MatDialogRef<ImportScenesDialogComponent>>;
   let sceneService: jasmine.SpyObj<ScenesService>;
+  let sceneServiceMock: jasmine.SpyObj<ScenesService>;
 
   beforeEach(async(() => {
     // tslint:disable-next-line:no-shadowed-variable
-    const dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
-    const sceneServiceMock = jasmine.createSpyObj('ScenesService', ['getScenes']);
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+    sceneServiceMock = jasmine.createSpyObj('ScenesService', ['getScenes']);
     TestBed.configureTestingModule({
       declarations: [ImportScenesDialogComponent],
       imports: [FormsModule, ReactiveFormsModule, MatDialogModule, TranslateModule.forRoot(), RouterTestingModule],
