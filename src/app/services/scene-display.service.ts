a88993ce786887d88b6bcdf14491fb0be7d81834
Yansarr <yanisgirardin@gmail.com>
2023-05-15 16:12:48 +0200

    Zoom in or zoom out an image

--- src/app/services/scene-display.service.ts
+++ src/app/services/scene-display.service.ts
@@ -20,14 +20,15 @@ export class SceneDisplayService {
   currImage = 0; // Variable used to reinitialize the canvas everytime the image is changed
   imageWidth: number;
   imageHeigth: number;
+  zoom = 1;
   hidePanel = false; // Variable used to hide or show the left panel
 
   UpdateDimensions() {
     this.onCanvasChange();
     let img = new Image();
     img.src = this.scenesService.SCENES[this.selectedScene].images[this.selectedImage].base64data;
-    this.imageWidth = img.width;
-    this.imageHeigth = img.height;
+    this.imageWidth = img.width * this.zoom;
+    this.imageHeigth = img.height * this.zoom;
 
     //variable to call an update on the canvas
     this.currImage++;
@@ -51,6 +52,23 @@ export class SceneDisplayService {
       this.imageWidth *= relation;
       this.imageHeigth *= relation;
     }
+  }
+
+  zoomInOrOut(zoom: string) {
+    let bigImageContainer: HTMLElement;
+    if(this.settingsService.AFSR){
+      bigImageContainer = document.getElementById('bigImageContainerAFSR');
+    }else{
+      bigImageContainer = document.getElementById('bigImageContainer');
+    }
+
+    if (zoom === "in" && this.imageWidth < bigImageContainer.clientWidth - 20 && this.imageHeigth < bigImageContainer.clientHeight - 20) {
+      this.zoom += 0.4;
+    } else if (zoom === "out" && this.imageWidth > 100 && this.imageHeigth > 100) {
+      this.zoom -= 0.4;
+    }
+    console.log(this.zoom);
+    this.UpdateDimensions();
   }
 
   onCanvasChange() {
